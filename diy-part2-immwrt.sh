e18e7efe61ea0e822290b426690593d714df30c3
welvery2010 <very20101@users.noreply.github.com>
2025-06-25 09:08:03 +0800

    Update diy-part2-immwrt.sh

--- diy-part2-immwrt.sh
+++ diy-part2-immwrt.sh
@@ -118,6 +118,9 @@ sed -i 's#mount -t cifs#mount.cifs#g' feeds/luci/applications/luci-app-cifs-moun
 #git clone -b main https://github.com/very20101/Openwrt_N1_try package/op-N1_try
 #mv package/op-N1_try/package_extra/luci-app-passwall feeds/smpackage/luci-app-passwall
 
+rm -rf feeds/packages/utils/kmod/Makefile
+git clone https://raw.githubusercontent.com/very20101/Actions_OpenWrt-Amlogic/refs/heads/main/patch/Makefile feeds/packages/utils/kmod/Makefile
+
 
 #rm -rf package/op-N1_try
 
